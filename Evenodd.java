0a97657e5b0602d151456e51fb9177dda202a3c5
dhivyabharathim <dhivyamariyappan19@gmail.com>
2016-08-24 17:27:57 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Evenodd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	int a;
+Scanner scanner=new Scanner(System.in);
+System.out.println("enter the value:");
+	a=scanner.nextInt();
+	if(a%2==0)
+	{
+		System.out.println("Even number");
+	}
+	else
+	{
+		System.out.println("odd number");
+	}
+	}
+	
+
+}
